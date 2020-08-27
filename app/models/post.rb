2d424d8c4209ef7625dff2dd6c7720d5bfbc3c1e
slurio <samantha.lurio@gmail.com>
2020-08-27 10:27:45 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |c|
+      if c[:name].strip != ""
+        category = Category.find_or_create_by(c)
+        self.categories << category
+      end
+    end
+  end
+
+  def unique_users
+    self.users.distinct
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Category.create(name: "style")
+Category.create(name: "sports")
+Category.create(name: "travel")
+
+Post.create(title: "Weekend Soccor Games", content: "I do not care really!")
+Post.create(title: "Fashion Week", content: "Is canceled this year and online!")
+Post.create(title: "Fall Styles", content: "Will be posted soon!")
+
+PostCategory.create(post_id: 1, category_id: 2)
+PostCategory.create(post_id: 2, category_id: 1)
+PostCategory.create(post_id: 3, category_id: 1)
+
+User.create(username: "slurio", email: "slur@gmail.com")
+
+Comment.create(content: "Great article! I agree", post_id: 1, user_id: 1)
+Comment.create(content: "Actually, this is a very bias article", post_id: 1, user_id: 1)
+Comment.create(content: "Can't wait for this article to be posted!", post_id: 3, user_id: 1)
